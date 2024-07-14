92e7701fa5f52556e5fc1c8f588aacd897d53d57
OleksandrYastremsky <ki23-o.yastremskyi@nubip.edu.ua>
2024-07-14 12:24:12 +0300

    Beta 0.1 (button and background)

--- betakino.py
+++ betakino.py
@@ -1,7 +1,18 @@
 #Відкриває додатковий звстосунок в якому має бути елементи керування 
 from tkinter import *
 
-window = Tk()
-window.title("Kinoshka")
-window.mainloop()
+kino = Tk()
+kino.title("Кіно")
+kino.geometry("1000x600")
+kino.configure(bg="#181818")
 
+button1 = Button(kino, text="Кіно 1", command=lambda: print("Кіно 1"))
+button2 = Button(kino, text="Кіно 2", command=lambda: print("Кіно 2"))
+
+# Розміщення кнопок
+button1.pack(pady=10)
+button2.pack(pady=10)
+
+
+
+kino.mainloop()
